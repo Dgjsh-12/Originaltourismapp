6eb6b95b1e6b2951ac8e5d0ed62e690d348815f1
EC2 Default User <ec2-user@ip-172-31-32-138.us-east-2.compute.internal>
2021-01-30 08:25:24 +0000

    オリジナル観光アプリ

---
+++ album/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Image(models.Model):
+    picture = models.ImageField(upload_to='images/')
+    title = models.CharField('タイトル', max_length=50)
+    text = models.CharField('内容', max_length=1000)
+
+    def __str__(self):
+        return self.title
